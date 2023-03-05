b7a0d3d8d63f76d8a6acf8593c9417e835b0a68a
aldiramdan <aldi.ramdan96@gmail.com>
2023-03-05 10:16:31 +0700

    fix validator

--- src/databases/orm/models/user.model.go
+++ src/databases/orm/models/user.model.go
@@ -8,14 +8,14 @@ type User struct {
 	UserID       string    `gorm:"primarykey; type:uuid; default:uuid_generate_v4()" json:"id,omitempty" valid:"-"`
 	Username     string    `json:"username,omitempty" valid:"type(string),required~Username is needed"`
 	Email        string    `json:"email" valid:"email,required~Email is needed"`
-	Password     string    `json:"password,omitempty"`
+	Password     string    `json:"password,omitempty" valid:"length(8|32), required~Password cannot be empty"`
 	Gender       string    `json:"gender,omitempty" valid:"-"`
 	Address      string    `json:"address,omitempty" valid:"-"`
 	DateOfBirth  string    `json:"date_of_birth,omitempty" valid:"-"`
 	Role         string    `gorm:"default:user" json:"role,omitempty" valid:"-"`
 	Image        string    `json:"image,omitempty" valid:"-"`
 	MobileNumber string    `json:"mobile_number,omitempty" valid:"-"`
-	TokenVerify  string    `json:"token_verify,omitempty" valid:"-"`
+	TokenVerify  string    `json:"token_verify" valid:"-"`
 	IsActive     bool      `gorm:"default: false" json:"is_active,omitempty" valid:"-"`
 	CreatedAt    time.Time `json:"created_at"  valid:"-"`
 	UpdatedAt    time.Time `json:"updated_at" valid:"-"`
